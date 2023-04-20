68c0c99ce2d7fdf12e9d2a6f21c855f6552dd887
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-04-20 18:09:00 +0200

    Sehenswürdigkeiten vorbereiten

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ let map = L.map("map").setView([
 
 // thematische Layer
 let themaLayer = {
-    stops: L.featureGroup().addTo(map),
-    lines: L.featureGroup().addTo(map),
-    zones: L.featureGroup().addTo(map),
+    stops: L.featureGroup(),
+    lines: L.featureGroup(),
+    zones: L.featureGroup(),
     sites: L.featureGroup().addTo(map)
 }
 
@@ -45,7 +45,7 @@ L.control.scale({
 async function showStops(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
-    console.log(response, jsondata);
+    //console.log(response, jsondata);
     L.geoJSON(jsondata).addTo(themaLayer.stops);
 }
 showStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
@@ -54,7 +54,7 @@ showStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&vers
 async function showLines(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
-    console.log(response, jsondata);
+    //console.log(response, jsondata);
     L.geoJSON(jsondata).addTo(themaLayer.lines);
 }
 showLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
@@ -63,7 +63,7 @@ showLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&vers
 async function showZones(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
-    console.log(response, jsondata);
+    //console.log(response, jsondata);
     L.geoJSON(jsondata).addTo(themaLayer.zones);
 }
 showZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
@@ -72,7 +72,7 @@ showZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&vers
 async function showSites(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
-    console.log(response, jsondata);
+    //console.log(response, jsondata);
     L.geoJSON(jsondata).addTo(themaLayer.sites);
 }
 showSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
