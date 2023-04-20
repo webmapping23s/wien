10adc3dc4c03f3c608b763119961d27ff80ef75e
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-04-20 18:36:23 +0200

    Popup für die Sehenswürdigkeiten

--- main.js
+++ main.js
@@ -76,8 +76,12 @@ async function showSites(url) {
     L.geoJSON(jsondata, {
         onEachFeature: function(feature, layer) {
             let prop = feature.properties;
-            layer.bindPopup(prop.NAME);
-            console.log(feature.properties, prop.NAME);
+            layer.bindPopup(`
+                <img src="${prop.THUMBNAIL}" alt="*">
+                <h4><a href="${prop.WEITERE_INF}" target="Wien">${prop.NAME}</a></h4>
+                <address>${prop.ADRESSE}</address>
+            `);
+            //console.log(feature.properties, prop.NAME);
         }
     }).addTo(themaLayer.sites);
 }
